ccdf7ba40d600274381f36e212f365291c159af5
Louis Coleman <coleloui18@gmail.com>
2021-03-04 11:59:14 -0800

    contact

--- src/component/BigFoot.js
+++ src/component/BigFoot.js
@@ -4,8 +4,10 @@ import { Avatar, Grid, Container, Hidden } from "@material-ui/core";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import DescriptionIcon from "@material-ui/icons/Description";
+import ContactMailIcon from "@material-ui/icons/ContactMail";
 import Resume from "./File/LouisColemanResume.pdf";
 import Louis from "./Photo/Louis.jpg";
+import Contact from "./contact";
 
 const useStyles = makeStyles((theme) => ({
   large: {
@@ -35,41 +37,73 @@ const useStyles = makeStyles((theme) => ({
 export default function BigFoot() {
   const classes = useStyles();
   return (
-    <Grid container justify="flex-start" alignItems="center" direction="row">
-      <Grid item>
-        <Grid container direction="column" justify="center" alignItems="center">
+    <Grid container alignItems="center" space={3} maxWidth="lg" direction="row">
+      <Grid item direction="column" sm={5}>
+        <Grid
+          container
+          alignItems="center"
+          direction="row"
+          justify="flex-start"
+        >
           <Grid item>
-            <a
-              className={classes.center}
-              target="_blank"
-              rel="noreferrer"
-              href="https://github.com/coleloui"
-            >
-              {" "}
-              <GitHubIcon fontSize="large" />
-            </a>
-          </Grid>
-          <Grid item>
-            <a
-              className={classes.center}
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.linkedin.com/in/louis-coleman/"
-            >
-              {" "}
-              <LinkedInIcon fontSize="large" />
-            </a>
-          </Grid>
-          <Grid item>
-            <a className={classes.center} download href={Resume}>
-              {" "}
-              <DescriptionIcon fontSize="large" />
-            </a>
+            <Grid container justify="flex-start" alignItems="center">
+              <Grid item>
+                <Grid
+                  container
+                  direction="column"
+                  justify="center"
+                  alignItems="center"
+                >
+                  <Grid item>
+                    <a
+                      className={classes.center}
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://github.com/coleloui"
+                    >
+                      {" "}
+                      <GitHubIcon fontSize="large" />
+                    </a>
+                  </Grid>
+                  <Grid item>
+                    <a
+                      className={classes.center}
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://www.linkedin.com/in/louis-coleman/"
+                    >
+                      {" "}
+                      <LinkedInIcon fontSize="large" />
+                    </a>
+                  </Grid>
+                  <Grid item>
+                    <a className={classes.center} download href={Resume}>
+                      {" "}
+                      <DescriptionIcon fontSize="large" />
+                    </a>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Avatar
+                  alt="Louis Coleman"
+                  src={Louis}
+                  className={classes.large}
+                />
+              </Grid>
+              <Grid item>
+                <ContactMailIcon fontSize="large" className={classes.center} />
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-      <Grid item>
-        <Avatar alt="Louis Coleman" src={Louis} className={classes.large} />
+      <Grid item size={5} direction="column">
+        <Grid container direction="row">
+          <Grid item>
+            <Contact />
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
--- src/component/contact.js
+++ src/component/contact.js
@@ -1,4 +1,25 @@
 import React from "react";
+import {
+  Grid,
+  Typography,
+  Input,
+  TextField,
+  InputAdornment,
+  FormControl,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import EmailIcon from "@material-ui/icons/Email";
+
+const useStyles = makeStyles((theme) => ({
+  new: {
+    fontWeight: "bold",
+    borderBottom: "5px solid #482880",
+  },
+  colorMe: {
+    color: "#482880",
+  },
+}));
 
 function getRandomString() {
   return (
@@ -10,7 +31,8 @@ function getRandomString() {
 const initialFormData = { name: "", email: "", message: "" };
 Object.freeze(initialFormData);
 
-export default function Contact(props) {
+export default function Contact() {
+  const classes = useStyles();
   const [contactSubject, setContactSubject] = React.useState("placeholder");
   const [formData, updateFormData] = React.useState({
     ...initialFormData,
@@ -25,14 +47,13 @@ export default function Contact(props) {
     });
   };
   return (
-    <div
-      id="contact"
-      className={`flex items-baseline col-start-${props.col} row-start-${props.row} self-center`}
-    >
-      <div className="container rounded-md border-4 border-teal-600 shadow-2xl bg-gray-400 mt-4">
-        <h1 className="underline text-center font-bold mt-2 mb-2">
-          Contact Me!
-        </h1>
+    <Grid container alignItems="center" direction="column" id="contact">
+      <Grid item>
+        <Typography variant="h3" className={classes.new}>
+          Lets Work Together!
+        </Typography>
+      </Grid>
+      <Grid item>
         <form
           onSubmit={async (e) => {
             e.preventDefault();
@@ -65,67 +86,105 @@ export default function Contact(props) {
             onChange={(e) => setContactSubject(e.target.value)}
             style={{ display: "none" }}
           />
-          <div className="mb-2 ml-4 mr-4">
-            <label htmlFor="name" className="block text-sm font-bold underline">
-              Name
-            </label>
-            <input
+          <Grid item container alignItems="center" direction="row">
+            <Grid item>
+              <Grid container direction="column">
+                <FormControl>
+                  <Input
+                    required
+                    name="name"
+                    type="text"
+                    value={formData.name}
+                    onChange={handleChange}
+                    startAdornment={
+                      <InputAdornment position="start">
+                        <AccountCircle className={classes.colorMe} />
+                      </InputAdornment>
+                    }
+                  />
+                </FormControl>
+                <FormControl>
+                  <Input
+                    required
+                    name="email"
+                    type="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    startAdornment={
+                      <InputAdornment position="start">
+                        <EmailIcon className={classes.colorMe} />
+                      </InputAdornment>
+                    }
+                  />
+                </FormControl>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <FormControl>
+                <TextField
+                  label="Give me some info!"
+                  multiline
+                  rows={4}
+                  name="message"
+                  onChange={handleChange}
+                  value={formData.message}
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+          {/* <input
               className="shadow appearance-none border rounded py-2 px-3 w-full mb-2 leading-tight focus:outline-none focus:shadow-outline"
               name="name"
               type="text"
               value={formData.name}
               placeholder="John Snow"
               onChange={handleChange}
-            />
-            <label
-              htmlFor="email"
-              className="block text-sm font-bold underline"
-            >
-              Email
-            </label>
-            <input
-              className="shadow appearance-none border rounded py-2 px-3 w-full mb-2 leading-tight focus:outline-none focus:shadow-outline"
-              name="email"
-              type="email"
-              value={formData.email}
-              placeholder="john.snow@placeholder.com"
-              onChange={handleChange}
-            />
-            <label
-              htmlFor="message"
-              className="block text-sm font-bold underline"
-            >
-              Message
-            </label>
-            <textarea
-              className="form-textarea block shadow appearance-none border rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline"
-              rows="5"
-              name="message"
-              value={formData.message}
-              placeholder="Hello!"
-              onChange={handleChange}
-            ></textarea>
-            <button
-              className="bg-teal-400 hover:bg-teal-600 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline border-4 border-teal-600"
-              type="submit"
-            >
-              Send
-            </button>
-            <div>
-              {bigWin === "success" && (
-                <div id="success">
-                  <p>Your message has been sent!</p>
-                </div>
-              )}
-              {bigWin === "error" && (
-                <div id="error">
-                  <p>An error occured while submitting the form.</p>
-                </div>
-              )}
-            </div>
+            /> */}
+          {/* <label htmlFor="email" className="block text-sm font-bold underline">
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded py-2 px-3 w-full mb-2 leading-tight focus:outline-none focus:shadow-outline"
+            name="email"
+            type="email"
+            value={formData.email}
+            placeholder="john.snow@placeholder.com"
+            onChange={handleChange}
+          /> */}
+          {/* <label
+            htmlFor="message"
+            className="block text-sm font-bold underline"
+          >
+            Message
+          </label> */}
+          {/* <textarea
+            className="form-textarea block shadow appearance-none border rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline"
+            rows="5"
+            name="message"
+            value={formData.message}
+            placeholder="Hello!"
+            onChange={handleChange}
+          ></textarea>
+          <button
+            className="bg-teal-400 hover:bg-teal-600 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline border-4 border-teal-600"
+            type="submit"
+          >
+            Send
+          </button> */}
+          <div>
+            {bigWin === "success" && (
+              <div id="success">
+                <p>Your message has been sent!</p>
+              </div>
+            )}
+            {bigWin === "error" && (
+              <div id="error">
+                <p>An error occured while submitting the form.</p>
+              </div>
+            )}
           </div>
         </form>
-      </div>
-    </div>
+      </Grid>
+    </Grid>
   );
 }
