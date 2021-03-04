ef1e6b3238bf00f562ac9b137206f1ffbf0a3ba4
Louis Coleman <coleloui18@gmail.com>
2021-03-03 19:39:29 -0800

    bio

--- src/component/bio.js
+++ src/component/bio.js
@@ -53,7 +53,6 @@ const useStyles = makeStyles((theme) => ({
 export default function Bio() {
   const classes = useStyles();
   return (
-<<<<<<< HEAD
     <Container className={classes.top} maxwidth="lg">
       <Grid container space={3} className={classes.top} maxwidth="lg">
         <Grid item sm={1} />
@@ -71,26 +70,6 @@ export default function Bio() {
             Express.js, React.js, Node.js, Database Theory, MongoDB, MySQL, CLI,
             and Git.
           </p>
-=======
-    <Container className={classes.top} maxWidth="lg">
-      <Grid container space={3}>
-        <Grid item sm={1}>
-          <Avatar
-            className={classes.img}
-            alt="Louis Coleman"
-            src="./photo/louis.jpg"
-          />
-        </Grid>
-        <Grid item sm={6} className={classes.bio}>
-          <Typography variant="p" color="initial">
-            Minim eiusmod elit ut qui aliqua veniam dolore incididunt est ex
-            irure. Adipisicing ad ullamco eu Lorem ut ea do incididunt aliquip
-            nostrud. Ipsum veniam enim et dolor ut. Incididunt cillum occaecat
-            excepteur veniam eu. Deserunt culpa qui id ex eiusmod minim.
-            Exercitation commodo incididunt tempor minim sit voluptate laboris
-            duis sit.
-          </Typography>
->>>>>>> 23993337762452400bfa02e8bfbc86f0198b3c1a
         </Grid>
         <Grid item sm={3} />
         <Hidden smDown>
