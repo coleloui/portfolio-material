12595bc4ec7155f64e624cde702a1d541f1c87d9
coleloui <coleloui18@gmail.com>
2021-09-13 08:08:20 -0700

    adding epic pages

---
+++ src/component/center/CenterEpic.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import { Typography, Grid, makeStyles, Slide, Hidden } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LaunchIcon from '@material-ui/icons/Launch';
+import RocketListPic from '../Photo/RocketList.png';
+import { RocketLinks } from '../ProjectLinks';
+
+const useStyles = makeStyles((theme) => ({
+  lines: {
+    lineHeight: 3,
+    textAlign: 'left',
+    marginLeft: '10px',
+    fontSize: '15px',
+  },
+  divider: {
+    borderLeft: '5px solid #482880',
+  },
+  tech: {
+    lineHeight: 2,
+    textAlign: 'left',
+    marginLeft: '10px',
+    fontSize: '15px',
+  },
+  jump: {
+    filter: 'drop-shadow(0.35rem 0.35rem 0.4rem rgba(0, 0, 0, 0.35))',
+    fontWeight: 'bold',
+    marginLeft: '10px',
+  },
+  large: {
+    marginTop: '20px',
+    marginRight: '20px',
+    marginLeft: theme.spacing(10),
+    width: theme.spacing(75),
+    height: 'auto',
+    [theme.breakpoints.down('lg')]: {
+      marginLeft: theme.spacing(5),
+      width: theme.spacing(42),
+      height: theme.spacing(47),
+    },
+    boxShadow: theme.shadows[15],
+    marginBottom: '10px',
+    borderRadius: '15px',
+  },
+  link: {
+    color: '#482880',
+    textDecoration: 'none',
+    filter: 'drop-shadow(0.35rem 0.35rem 0.4rem rgba(0, 0, 0, 0.5))',
+    '&:hover': {
+      color: 'gray',
+      textDecoration: 'underline',
+    },
+  },
+  center: {
+    color: '#482880',
+    marginLeft: '25px',
+    filter: 'drop-shadow(0.35rem 0.35rem 0.4rem rgba(0, 0, 0, 0.25))',
+    fontSize: 50,
+    '&:hover': {
+      color: 'gray',
+    },
+  },
+}));
+
+export default function RocketList() {
+  const classes = useStyles();
+
+  const RocketList = useSelector((state) => state.RocketList);
+
+  return (
+    <Slide direction='right' in={RocketList} mountOnEnter unmountOnExit>
+      <Grid container direction='row' alignItems='center'>
+        <Hidden mdDown>
+          <Grid item>
+            <a
+              rel='noopener noreferrer'
+              target='_blank'
+              href='http://rocketlist.herokuapp.com/'
+            >
+              <img
+                src={RocketListPic}
+                alt='RocketList home'
+                className={classes.large}
+              />
+            </a>
+          </Grid>
+        </Hidden>
+        <Grid item xs={9} md={8} lg={5}>
+          <Grid container direction='column' className={classes.divider}>
+            <Grid item>
+              <Typography className={classes.jump} variant='h3'>
+                RocketList{' '}
+                <a
+                  href='https://github.com/meganjacobs97/RocketList'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <GitHubIcon className={classes.center} />
+                </a>
+                <a
+                  href='http://rocketlist.herokuapp.com/'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <LaunchIcon className={classes.center} />
+                </a>
+              </Typography>
+              <p className={classes.lines}>
+                This application is a React website built with inspiration from
+                Craigslist, Yahoo Answers, and Reddit. It allows for different
+                topics and for people to make a post where others can make
+                comments. We also added a feature that allows users to join chat
+                rooms within a specific post where they can discuss the topic at
+                hand in more depth.
+              </p>
+            </Grid>
+            <Grid item>
+              <Typography variant='h4' className={classes.jump}>
+                Built Using:
+              </Typography>
+              <p className={classes.tech}>
+                {RocketLinks.map(({ tech, link, ending }) => (
+                  <span>
+                    <a
+                      key={tech}
+                      rel='noopener noreferrer'
+                      className={classes.link}
+                      target='_blank'
+                      href={link}
+                    >
+                      {tech}
+                    </a>
+                    {ending}
+                  </span>
+                ))}
+              </p>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Slide>
+  );
+}
---
+++ src/component/right/Epic.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { makeStyles, Grid, Typography, Grow } from "@material-ui/core";
+import { useSelector, useDispatch } from "react-redux";
+import FullscreenIcon from "@material-ui/icons/Fullscreen";
+import RocketList from "../Photo/RocketList.png";
+import { MAPLEVALLEY, BIO, ROCKETLIST, POKEGODEX } from "../../actions";
+
+const useStyles = makeStyles((theme) => ({
+  size: {
+    [theme.breakpoints.up("sm")]: {
+      height: theme.spacing(25),
+      width: theme.spacing(55),
+    },
+    filter: "drop-shadow(0.35rem 0.35rem 0.4rem rgba(0, 0, 0, 0.5))",
+    borderRadius: "15px",
+    backgroundImage: `url(${RocketList})`,
+    backgroundSize: "cover",
+    "&:hover": {
+      opacity: 0.5,
+    },
+    "&:hover svg": {
+      visibility: "visible",
+    },
+  },
+  looks: {
+    fontWeight: "bold",
+    filter: "drop-shadow(0.35rem 0.35rem 0.4rem rgba(0, 0, 0, 0.5))",
+  },
+  walls: {
+    marginRight: theme.spacing(5),
+    marginBottom: theme.spacing(5),
+  },
+  noshow: {
+    visibility: "hidden",
+    height: theme.spacing(10),
+    width: theme.spacing(10),
+    display: "block",
+    margin: "auto",
+    marginTop: "50px",
+    color: "#482880",
+  },
+}));
+
+export default function Epic() {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const BioRight = useSelector((state) => state.BioRight);
+  const RocketListRight = useSelector((state) => state.RocketListRight);
+  const MapleValleyRight = useSelector((state) => state.MapleValleyRight);
+  const PokeGoDexRight = useSelector((state) => state.PokeGoDexRight);
+
+  const moveRocketList = () => {
+    if (BioRight === false) {
+      dispatch(BIO());
+      dispatch(ROCKETLIST());
+    } else if (MapleValleyRight === false) {
+      dispatch(MAPLEVALLEY());
+      dispatch(ROCKETLIST());
+    } else if (PokeGoDexRight === false) {
+      dispatch(POKEGODEX());
+      dispatch(ROCKETLIST());
+    } else dispatch(ROCKETLIST());
+  };
+
+  return (
+    <Grow in={RocketListRight}>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        className={classes.walls}
+      >
+        {" "}
+        <Typography variant="h4" className={classes.looks}>
+          RocketList
+        </Typography>
+        <div
+          title="RocketList"
+          className={classes.size}
+          onClick={() => {
+            moveRocketList();
+          }}
+        >
+          <FullscreenIcon className={classes.noshow} />
+        </div>
+      </Grid>
+    </Grow>
+  );
+}
